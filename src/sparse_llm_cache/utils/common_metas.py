469c536c869be086894dc9e8928ecd21bc3e8779
xiaoniu.sxn <xiaoniu.sxn@outlook.com>
2024-08-08 10:51:33 +0000

    add metadata for qwen2 57b and deepseek v2 lite

--- src/sparse_llm_cache/utils/common_metas.py
+++ src/sparse_llm_cache/utils/common_metas.py
@@ -66,6 +66,32 @@ class ModelMetas:
     )
     return asdict(ret) if return_dict else ret
 
+  def build_deepseek_v2_lite(return_dict = True):
+    def parse_moe_layer_id(name):
+      match = re.match(r'.*layers\.(\d+).*', name)
+      if not match:
+        return None
+      return int(match.group(1)) - 1 # first layer is not moe layer
+    def parse_expert_id(name):
+      match = re.match(r'.*layers\.\d+\.mlp\.experts\.(\d+).*$', name)
+      if not match:
+        return None
+      return int(match.group(1))
+
+    def parse_expert_meta_from_name(name):
+      return (parse_moe_layer_id(name), parse_expert_id(name))
+    ret = ModelMetas(
+      num_moe_layer         = 26,
+      num_expert_per_layer  = 64,
+      num_expert_per_token  = 6,
+      expert_meta_parser    = parse_expert_meta_from_name,
+      expert_name_filter    = RegexFilter(r'.*layers\.(\d+)\.mlp\.experts\.(\d+)$'),
+      moe_attn_name_filter  = RegexFilter(r'.*layers\.([1-9]\d*)\.self_attn$'),
+      moe_mlp_name_filter   = RegexFilter(r'.*layers\.([1-9]\d*)\.mlp$'),
+      moe_layer_name_filter = RegexFilter(r'.*layers\.([1-9]\d*)$')
+    )
+    return asdict(ret) if return_dict else ret
+
   @staticmethod
   def build_qwen_moe(return_dict = True):
     def parse_moe_layer_id(name):
@@ -85,6 +111,33 @@ class ModelMetas:
       num_moe_layer         = 24,
       num_expert_per_layer  = 60,
       num_expert_per_token  = 4,
+      expert_meta_parser    = parse_expert_meta_from_name,
+      expert_name_filter    = RegexFilter(r'.*layers\.(\d+)\.mlp\.experts\.(\d+)$'),
+      moe_attn_name_filter  = RegexFilter(r'.*layers\.(\d+)\.self_attn$'),
+      moe_mlp_name_filter   = RegexFilter(r'.*layers\.(\d+)\.mlp$'),
+      moe_layer_name_filter = RegexFilter(r'.*layers\.(\d+)$')
+    )
+    return asdict(ret) if return_dict else ret
+
+  @staticmethod
+  def build_qwen2_57b_a14b(return_dict = True):
+    def parse_moe_layer_id(name):
+      match = re.match(r'.*layers\.(\d+).*', name)
+      if not match:
+        return None
+      return int(match.group(1))
+    def parse_expert_id(name):
+      match = re.match(r'.*layers\.\d+\.mlp\.experts\.(\d+).*$', name)
+      if not match:
+        return None
+      return int(match.group(1))
+
+    def parse_expert_meta_from_name(name):
+      return (parse_moe_layer_id(name), parse_expert_id(name))
+    ret = ModelMetas(
+      num_moe_layer         = 28,
+      num_expert_per_layer  = 64,
+      num_expert_per_token  = 8,
       expert_meta_parser    = parse_expert_meta_from_name,
       expert_name_filter    = RegexFilter(r'.*layers\.(\d+)\.mlp\.experts\.(\d+)$'),
       moe_attn_name_filter  = RegexFilter(r'.*layers\.(\d+)\.self_attn$'),
@@ -124,7 +177,10 @@ class ModelMetas:
 predefined_metas = {
   'deepseek-ai/deepseek-moe-16b-chat'          : ModelMetas.build_deepseek_moe,
   'deepseek-ai/deepseek-moe-16b-chat-simulate' : ModelMetas.build_deepseek_moe_simulate,
+  'deepseek-ai/DeepSeek-V2-Lite-Chat'          : ModelMetas.build_deepseek_v2_lite,
   'Qwen/Qwen1.5-MoE-A2.7B-Chat'                : ModelMetas.build_qwen_moe,
+  'Qwen/Qwen2-57B-A14B-Instruct'               : ModelMetas.build_qwen2_57b_a14b,
+  'Qwen/Qwen2-57B-A14B-Instruct-GPTQ-Int4'     : ModelMetas.build_qwen2_57b_a14b,
   'mistralai/Mixtral-8x7B-Instruct-v0.1'       : ModelMetas.build_mixtral,
   'TheBloke/Mixtral-8x7B-Instruct-v0.1-GPTQ'   : ModelMetas.build_mixtral,
 }
