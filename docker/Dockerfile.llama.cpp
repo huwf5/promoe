63481e05c2f6220eeb71bcf4887c0ebd1a395af9
xiaoniu.sxn <xiaoniu.sxn@outlook.com>
2024-09-25 13:25:48 +0000

    minor

--- docker/Dockerfile.llama.cpp
+++ docker/Dockerfile.llama.cpp
@@ -15,7 +15,6 @@ RUN apt-get update && \
     apt-get install -y build-essential python3 python3-pip git libcurl4-openssl-dev libgomp1 zsh curl wget vim ccache
 
 COPY requirements.txt   requirements.txt
-COPY requirements       requirements
 
 RUN pip install --upgrade pip setuptools wheel pandas \
     && pip install -r requirements.txt \
