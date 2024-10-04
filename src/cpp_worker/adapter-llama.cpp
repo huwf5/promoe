e5bd17db692c982e2d40179a76364a83fbc56e82
xiaoniu.sxn <xiaoniu.sxn@outlook.com>
2024-10-04 05:10:04 +0000

    refine gpu_mem_info api

--- src/cpp_worker/adapter-llama.cpp
+++ src/cpp_worker/adapter-llama.cpp
@@ -1,12 +1,12 @@
 #include "adapter-llama.hpp"
 #include "logging.hpp"
 extern "C" {
-void get_gpu_mem_info(size_t *free_byte, size_t *total_byte) {
+void get_gpu_mem_info(uint64_t *free_byte, uint64_t *total_byte) {
   CUDA_CALL(cudaMemGetInfo(free_byte, total_byte));
 }
 
 void log_gpu_mem_info() {
-  size_t free_byte, total_byte;
+  uint64_t free_byte, total_byte;
   get_gpu_mem_info(&free_byte, &total_byte);
   LOG(ERROR) << "GPU memory: free " << free_byte/1024.0/1024.0/1024.0 << "GB, total " << total_byte/1024.0/1024.0/1024.0 << "GB";
 }
--- src/cpp_worker/adapter-llama.hpp
+++ src/cpp_worker/adapter-llama.hpp
@@ -3,6 +3,6 @@
 #include "profiler.hpp"
 
 extern "C" {
-  void get_gpu_mem_info(size_t* free, size_t* total);
+  void get_gpu_mem_info(uint64_t* free, uint64_t* total);
   void log_gpu_mem_info();
 }
